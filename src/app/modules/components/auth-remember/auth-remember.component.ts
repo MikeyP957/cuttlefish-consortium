95120a53b85eefed54f3062b67e593ea74e613cc
MikeyP957 <peraram2@gmail.com>
2022-03-21 13:40:34 -0700

    remember user component

---
+++ src/app/modules/components/auth-remember/auth-remember.component.ts
@@ -0,0 +1,19 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'auth-remember',
+  templateUrl: './auth-remember.component.html',
+  styleUrls: ['./auth-remember.component.sass']
+})
+export class AuthRememberComponent {
+
+  constructor() { }
+
+  @Output() checked: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  onChecked(value: boolean) {
+    this.checked.emit(value);
+  }
+
+
+}
