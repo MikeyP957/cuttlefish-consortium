e2e77406d75a4c45f7ee81f68b7c5422d5fb1de5
MikeyP957 <peraram2@gmail.com>
2022-03-24 21:11:44 -0700

    Authorization form module

--- src/app/modules/auth-form-module/auth-form.module.ts
+++ src/app/modules/auth-form-module/auth-form.module.ts
@@ -1,22 +1,18 @@
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
 
-import { AuthFormComponent } from '../components/auth-form/auth-form.component';
-import { AuthRememberComponent } from '../components/auth-remember/auth-remember.component';
+import { AuthFormComponent } from "../components/auth-form/auth-form.component";
+import { AuthRememberComponent } from "../components/auth-remember/auth-remember.component";
+import { AuthMessageComponent } from "../components/auth-message/auth-message.component";
 
 @NgModule({
   declarations: [
     AuthFormComponent,
-    AuthRememberComponent
+    AuthRememberComponent,
+    AuthMessageComponent,
   ],
-  imports: [
-    CommonModule,
-    FormsModule
-  ],
-  exports: [
-    AuthFormComponent,
-    AuthRememberComponent
-  ]
+  imports: [CommonModule, FormsModule],
+  exports: [AuthFormComponent, AuthRememberComponent, AuthMessageComponent],
 })
-export class AuthFormModule { }
+export class AuthFormModule {}
